c0c82f87060a6ec93fc5971268e3123c289d793d
jasimahmedq2 <jasimahmed90852417>
2023-01-16 20:48:12 +0600

    update post data

--- controller/newPost.controller.js
+++ controller/newPost.controller.js
@@ -1,10 +1,23 @@
 const postModel = require("../Model/post.model")
 
-exports.createPostController = async(req, res) => {
+exports.createPostController = async (req, res) => {
   try {
     const newPost = new postModel(req.body)
     const result = await newPost.save()
     res.status(200).send('successfully create a post')
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+exports.updatePostController = async (req, res) => {
+  try {
+    const post = await postModel.findById(req.params.id)
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body })
+      res.status(200).send('successfully update the post',)
+    } else {
+      res.status(403).send("you can't update another user post")
+    }
   } catch (error) {
     res.status(500).send(error)
   }
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createPostController } = require('../controller/newPost.controller');
+const { createPostController, updatePostController } = require('../controller/newPost.controller');
 const Router = express.Router();
 
 Router.post('/', createPostController)
+Router.put('/:id', updatePostController)
 
 module.exports = Router;
